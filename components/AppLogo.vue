8a195e035edcd137266a134375032b2678d4049b
Rolando Espinosa <reel.leer@outlook.com>
2022-02-17 17:11:40 -0600

    Logo component

---
+++ components/AppLogo.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtLink to="/" class="logo">
+    <img height="32px" :src="logoPath" alt="Logo de ABACO">
+  </NuxtLink>
+</template>
+<script setup>
+const props = defineProps({
+  color: {
+    type: String,
+    required: false,
+    default: "normal",
+    validator: (value) => ["light", "normal","white"].includes(value)
+  }
+})
+
+const logoPath =
+  `/abaco_logo${ props.color === "normal" ? "" : "_" + props.color }.png`
+</script>
+<style>
+.logo {
+  line-height: 0;
+}
+</style>
