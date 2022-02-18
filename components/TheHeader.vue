98d8bad07c9a80e89c07966fb475f14124774973
Rolando Espinosa <reel.leer@outlook.com>
2022-02-18 16:55:50 -0600

    Menu component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,13 +2,7 @@
   <header class="header">
     <div class="container">
       <AppLogo color="light" />
-      <ul class="header__menu">
-        <li>Inicio</li>
-        <li>Nosotros</li>
-        <li>Servicios</li>
-        <li>Clientes</li>
-        <li>Contacto</li>
-      </ul>
+      <TheMenu class="header__menu" />
       <svg class="header__mobile-icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
       </svg>
@@ -37,7 +31,8 @@
   display: block;
   left: 0px;
   position: absolute;
-  top: 55px;
+  top: 54px;
+  width: 100%;
 }
 
 .header__mobile-icon {
