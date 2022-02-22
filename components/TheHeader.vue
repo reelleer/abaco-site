524af88763ca819e8b80dbb08bb4ad7e2637f5be
Rolando Espinosa <reel.leer@outlook.com>
2022-02-22 16:30:30 -0600

    The menu component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,13 +2,22 @@
   <header class="header">
     <div class="container">
       <AppLogo color="light" />
-      <TheMenu class="header__menu" />
-      <svg class="header__mobile-icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+      <TheMenu class="header__menu" :class="menuClass" />
+      <svg class="header__mobile-icon" @click="toggleMenu" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
       </svg>
     </div>
   </header>
 </template>
+<script setup>
+import { ref, computed } from "vue"
+
+const isOpen = ref(false)
+
+const menuClass = computed(() => isOpen.value ? "header__menu--display" : "")
+
+const toggleMenu = () => isOpen.value = !isOpen.value
+</script>
 <style>
 .header {
   background-color: var(--background-color);
