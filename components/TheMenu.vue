8f84aaaa7ab72b9af6cb287c9d07cb341f7816dc
Rolando Espinosa <reel.leer@outlook.com>
2022-02-22 17:56:30 -0600

    The menu component

---
+++ components/TheMenu.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="nav">
+    <ul>
+      <li>Inicio</li>
+      <li>Nosotros</li>
+      <li>Servicios</li>
+      <li>Clientes</li>
+      <li>Contacto</li>
+    </ul>
+  </nav>
+</template>
+<style>
+.nav {
+  background: inherit;
+}
+</style>
