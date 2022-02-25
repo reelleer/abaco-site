c15b2f1269c0d778b1e37e4c39ba6a34d4560979
Rolando Espinosa <reel.leer@outlook.com>
2022-02-24 18:01:09 -0600

    Styling TheMenu component

--- components/TheMenu.vue
+++ components/TheMenu.vue
@@ -1,16 +1,44 @@
 <template>
-  <nav class="nav">
-    <ul>
-      <li>Inicio</li>
-      <li>Nosotros</li>
-      <li>Servicios</li>
-      <li>Clientes</li>
-      <li>Contacto</li>
+  <nav class="navbar">
+    <ul class="navbar__list">
+      <li v-for="link in links" :key="link.path" class="navbar__list-item">
+        <NuxtLink :to="link.path">
+        {{ link.name }}
+        </NuxtLink>
+      </li>
     </ul>
   </nav>
 </template>
+<script setup>
+const links = [
+  { path: "/", name: "Inicio" },
+  { path: "/about", name: "Nosotros" },
+  { path: "/services", name: "Servicios" },
+  { path: "/clients", name: "Clientes" },
+  { path: "/contact", name: "Contacto" },
+]
+</script>
 <style>
-.nav {
-  background: inherit;
+.navbar {
+  background: var(--background-color);
+  color: white;
+  height: calc(100vh - 54px)
+}
+
+.navbar__list {
+  list-style: none;
+  padding-left: 1rem;
+  font-size: 1.5rem;
+}
+
+.navbar__list-item a {
+  color: white;
+  display: inline-block;
+  font-size: 1.25rem;
+  padding: 0.75rem 0 0.75rem 0;
+}
+
+.navbar__list-item .router-link-active {
+  border-top: 0.25rem solid var(--background-color-emphasis);
 }
 </style>
