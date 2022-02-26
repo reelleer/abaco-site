df47801514321fd311fa90b7f036d3a7cbbff453
Rolando Espinosa <reel.leer@outlook.com>
2022-02-25 21:23:55 -0600

    Adding AppIcon component

---
+++ components/AppIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg width="16" height="16" fill="currentColor">
+    <use v-bind="attrs" />
+  </svg>
+</template>
+<script setup>
+import { ref, computed } from "vue"
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+    validator: (value) => 
+      ["envelope", "geo-alt", "telephone", "list"].includes(value)
+  }
+})
+
+const name = ref(props.name)
+
+const attrs = computed(() => { return { href: `#${name.value}` } }) 
+</script>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -5,12 +5,18 @@
       Sobre ABACO. Somos un equipo de profesionales que promovemos una cultura de
       efectividad en empresas y queremos apoyarlos.
       </p>
-      <p>
-      Edificio Kakau, Camino de oriente
-      piso 8 Modulo A-801<br />
-      +505 8810-2929<br />
-      abaco@abaco.com.ni
-      </p>
+      <ul>
+        <li>
+          <AppIcon name="geo-alt" /> Edificio Kakau, Camino de oriente piso 8
+          Modulo A-801
+        </li>
+        <li>
+          <AppIcon name="telephone" /> +505 8810-2929<br />
+        </li>
+        <li>
+          <AppIcon name="envelope" /> abaco@abaco.com.ni
+        </li>
+      </ul>
       <AppLogo color="white" />
     </div>
   </footer>
