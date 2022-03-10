f6647c903365a23444a2f91a293c96e4731ec249
Rolando Espinosa <reel.leer@outlook.com>
2022-03-09 18:29:49 -0600

    Styling footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,22 +1,24 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <p>
-      Sobre ABACO. Somos un equipo de profesionales que promovemos una cultura de
-      efectividad en empresas y queremos apoyarlos.
-      </p>
-      <ul class="list-icon">
-        <li>
-          <AppIcon name="geo-alt" /> Edificio Kakau, Camino de oriente piso 8
-          Modulo A-801
-        </li>
-        <li>
-          <AppIcon name="telephone" /> +505 8810-2929<br />
-        </li>
-        <li>
-          <AppIcon name="envelope" /> abaco@abaco.com.ni
-        </li>
-      </ul>
+      <div>
+        <p>
+        Sobre ABACO. Somos un equipo de profesionales que promovemos una cultura de
+        efectividad en empresas y queremos apoyarlos.
+        </p>
+        <ul class="list-icon">
+          <li>
+            <AppIcon name="geo-alt" /> Edificio Kakau, Camino de oriente piso 8
+            Modulo A-801
+          </li>
+          <li>
+            <AppIcon name="telephone" /> +505 8810-2929<br />
+          </li>
+          <li>
+            <AppIcon name="envelope" /> abaco@abaco.com.ni
+          </li>
+        </ul>
+      </div>
       <div class="footer__logo">
         <AppLogo color="white" />
       </div>
@@ -30,6 +32,17 @@
 
 .footer__logo {
   text-align: center;
+}
+
+@media only screen and (min-width: 768px) {
+  .footer .container {
+    display: flex;
+    gap: 1rem;
+  }
+
+  .footer__logo {
+    align-self: center;
+  }
 }
 </style>
 
