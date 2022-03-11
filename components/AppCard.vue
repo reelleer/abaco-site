b982140f462ab0cffaf99ca17957e1faf94405af
Psychocrazy <larioshamilton@gmail.com>
2022-03-11 16:03:50 -0600

    Show services

---
+++ components/AppCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="card">
+        <slot name="media"></slot>
+        <div class="card__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+
+
+
+<style>
+.card{
+	width: 90%;
+	margin: 20px;
+	border-radius: 10px;
+	overflow: hidden;
+	background: #173F58;
+	box-shadow: 0px 1px 10px rgha(0,0,0,0.2);
+	cursor: default;
+}
+
+.card:hover{
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, 4);
+	
+}
+
+.card img{
+	width: 100%;
+	height: 200px;
+}
+
+.card {
+	padding:15px;
+	text-align:center;
+    font-family: Helvetica;
+
+}
+.card .card__content h3{
+    color:#fff;
+	font-weight: 400;
+	margin-bottom: 15px;
+
+}
+.card .card__content p{
+	line-height: 1.5;
+	color: #fff;
+}
+
+.card .card__content a{
+	text-decoration: none;
+	display: inline-block;
+    padding: 10px;
+    margin-top: 10px;
+    color:#fff;
+    border-radius: 10px;
+    background: #05A053;
+    
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,66 +4,10 @@
 			<h3>Este es el espacio para el Banner</h3>
 		</section>
 		<section>
-			<div>
-				<h2>Estamos aqui para ayudarte<br>descubre nuestros servicios</h2>
-			</div>
-			<div class="card">
-				<figure class="card__image">
-				<!--<img src="assets/img/services-finance.jpg">-->	
-				</figure>
-				<div class="card_content">
-					<h3>Finanzas</h3>
-					<p>El rol de las finanzas es la clave para la alta gerencia y para la toma de decisiones en general</p>
-					<a href="#">Ver mas</a>
-				</div>
-			</div>
+			<PagesIndexServices>
+
+			</PagesIndexServices>
 		
-			<div class="card">
-				<figure class="card">
-				<!--<img src="assets/img/services-recruitment.jpg">-->	
-				</figure>
-				<div class="contenido">
-					<h3>Busqueda de Personal Ejecutivo</h3>
-					<p>Apoyamos a encontrar al personal idoneo para los puestos que necesita llenar.</p>
-					<a href="#">Ver mas</a>
-				</div>
-			</div>
-			<div class="card">
-				<figure class="card">
-				<!--<img src="assets/img/services-development.jpg">-->	
-				</figure>
-				<div class="contenido">
-					<h3>Desarrollo Organizativo</h3>
-					<p>El cambio no es un fin, si no un medio para enfrentar la competencia. </p>
-					<form action="">
-						<button type="submit">Ver Mas</button>
-					</form>
-				</div>
-			</div>
-			<div class="card">
-				<figure class="card">
-				<!--<img src="assets/img/services-client.jpg">-->	
-				</figure>
-				<div class="contenido">
-					<h3>Servicio al Cliente</h3>
-					<p>Ayudamos a las empresas a evaluar la calidad del servicio que estan brindando a sus clientes. </p>
-					<form action="">
-						<button type="submit">Ver Mas</button>
-					</form>
-				</div>
-			</div>
-			<div class="card">
-				<figure class="card">
-				<!--<img src="assets/img/services-mentoring.jpg">-->	
-				</figure>
-				<div class="contenido">
-					<h3>Gestion Humana</h3>
-					<p>La gestion humana es una especialidad estrategica en el manejo de las empresas. </p>
-					<form action="">
-						<button type="submit">Ver Mas</button>
-					</form>
-				</div>
-			</div>
 		</section>
 		<section>
 			<div>
